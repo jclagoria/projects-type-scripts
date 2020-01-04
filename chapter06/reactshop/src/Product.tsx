f11ab7ae3bd1e419fcaa72ba09015790a89cbf41
jclagoria <lagoria79@gmail.com>
2020-01-04 19:12:52 -0300

    Container and presentational components

---
+++ chapter06/reactshop/src/Product.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+import { IProduct } from "./ProductsData";
+
+interface IProps {
+    product: IProduct;
+    inBasket: boolean;
+    onAddToBasket: () => void;
+}
+
+const Product: React.FC<IProps> = props => {
+
+  const product = props.product;
+
+  const handleAddClick = () => {
+      props.onAddToBasket();
+  };
+
+  return (<React.Fragment>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p className="product-price">
+          {
+              new Intl.NumberFormat("en-US", {
+                  currency: "USD",
+                  style: "currency"
+              }).format(product.price)
+          }
+      </p>
+      {!props.inBasket && (
+          <button onClick={handleAddClick}>
+              Add to the basket
+          </button>
+      )}
+            </React.Fragment>);
+};
+
+export default Product;
