46020e82dcd451ee32aaaf00e515413dbcc16474
jclagoria <lagoria79@gmail.com>
2020-01-07 22:36:38 -0300

    Implementing our new ContactUs component

--- chapter07/01-CreatingAFormWithControlledComponents/src/ContactUs.tsx
+++ chapter07/01-CreatingAFormWithControlledComponents/src/ContactUs.tsx
@@ -1,60 +1,18 @@
 import * as React from "react";
-import {mkdtemp} from "fs";
 
-interface IProps {
-    name: string;
-    onNameChange: (name: string) => void;
-    email: string;
-    onEmailChange: (email: string)  => void;
-    reason: string;
-    onReasonChange: (reason: string) => void;
-    notes: string;
-    onNotesChange: (notes: string) => void;
-}
+import { Form} from "./Form";
 
-const ContactUs: React.FC<IProps> = props => {
-
-    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        props.onNameChange(e.currentTarget.value);
-    };
-
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        props.onEmailChange(e.currentTarget.value);
-    };
-
-    const handleReasonChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        props.onReasonChange(e.currentTarget.value);
-    };
-
-    const handleNotesChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        props.onNotesChange(e.currentTarget.value);
-    };
-
+const ContactUs: React.FC = () => {
     return (
-        <form className="form" noValidate={true}>
-            <div className="form-group">
-                <label htmlFor="name">Your name</label>
-                <input type="text" id="name" value={props.name} onChange={handleNameChange}/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="email">Your email address</label>
-                <input type="email" id="email" value={props.email} onChange={handleEmailChange}/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="reason">Reason you need to contact us</label>
-                <select id="reason" value={props.reason} onChange={handleReasonChange}>
-                    <option value="Marketing">Marketing</option>
-                    <option value="Support" selected={true}>Support</option>
-                    <option value="Feedback">Feedback</option>
-                    <option value="Jobs">Jobs</option>
-                    <option value="Other">Other</option>
-                </select>
-            </div>
-            <div className="form-group">
-                <label htmlFor="notes">Additional notes</label>
-                <textarea id="notes" value={props.notes} onChange={handleNotesChange} />
-            </div>
-        </form>
+        <Form defaultValules={{name: "", email: "", reason: "Support", notes: ""}}>
+            <Form.Field label="Your name" name="name" />
+            <Form.Field label="Your email address" name="email" type="Email" />
+            <Form.Field label="Reason you need to contact us"
+                        name="reason" type="Select"
+                        options={["Marketing", "Support", "Jobs", "Other"]} />
+            <Form.Field label="Additional notes"
+                        name="notes" type="TextArea" />
+        </Form>
     );
 };
 
