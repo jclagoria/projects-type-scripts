7c316e4028c84c49ad986f38b92df16fb7ffa34d
jclagoria <lagoria79@gmail.com>
2020-01-09 02:01:50 -0300

    Adding the store Provider component

--- chapter08/src/index.tsx
+++ chapter08/src/index.tsx
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Routes from "./Routes";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { Store } from "redux";
+
+import "./index.css";
+
+import Routes from "./Routes";
+import configureStore from "./Store";
+import { IApplicationState } from "./Store";
+
+interface IProps {
+    store: Store<IApplicationState>;
+}
+
+const Root: React.FC<IProps> = props => {
+    return (
+        <Provider store={props.store}>
+            <Routes />
+        </Provider>
+    );
+};
+
+const store = configureStore();
+
+ReactDOM.render(<Root store={store} />,
+    document.getElementById("root") as HTMLElement);
