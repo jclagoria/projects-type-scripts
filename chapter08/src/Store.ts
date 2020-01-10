74eec65f51f5c948e46e3106edf43e5b44eac2bd
jclagoria <lagoria79@gmail.com>
2020-01-10 19:47:30 -0300

    Adding basket state and actions to the store

--- chapter08/src/Store.ts
+++ chapter08/src/Store.ts
@@ -3,14 +3,18 @@ import { applyMiddleware, combineReducers, createStore, Store } from "redux";
 import thunk from "redux-thunk";
 
 import { productsReducer } from "./ProductReducer";
+import { basketReducer } from "./BasketReducer";
 import { IProductsState } from "./ProductsType";
+import { IBasketState } from "./BasketTypes";
 
 export interface IApplicationState {
     products: IProductsState;
+    basket: IBasketState;
 }
 
 const rootReducer = combineReducers<IApplicationState>({
-    products: productsReducer
+    products: productsReducer,
+    basket: basketReducer
 });
 
 export default function configureStore(): Store<IApplicationState> {
